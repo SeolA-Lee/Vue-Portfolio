508e4b70b3ef8002c4eed0488c781273a9698d74
SeolA-Lee <dlcodus1201@naver.com>
2023-12-16 17:48:34 +0900

    vue 8th

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '<%= BASE_URL %>/Vue-Portfolio/'
+  publicPath: '/Vue-Portfolio'
 })
